cd9e99a13d1eea8829ad0aa2f5ae279ce7725a8e
Paryszek <michal.parysz@yahoo.com>
2019-05-13 13:19:50 +0200

    upload s3 public policy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Storage } from 'aws-amplify'
 
 @Component({
   selector: 'app-root',
@@ -9,10 +10,18 @@ export class AppComponent {
   title = 'AWSApplication';
 
   onImagePicked($event: any) {
-    debugger;
   }
 
-  onImageLoaded($event: any) {
-    debugger;
+  onImageLoaded(e: any) {
+    const file = e;
+      Storage.put('example.png', file, {
+          contentType: 'image/png',
+          level: 'protected',
+          progressCallback(progress) {
+            console.log(`Uploaded: ${progress.loaded}/${progress.total}`);
+          }
+      })
+      .then (result => console.log(result))
+      .catch(err => console.log(err));
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -7,6 +7,20 @@ import Amplify from 'aws-amplify';
 import amplify from './aws-exports';
 
 Amplify.configure(amplify);
+// Amplify.configure({
+//   Auth: {
+//       identityPoolId: 'eu-west-2:cd50d3b1-f397-478b-a4e6-cf138218bf4b', //REQUIRED - Amazon Cognito Identity Pool ID
+//       region: 'eu-west-2', // REQUIRED - Amazon Cognito Region
+//       userPoolId: 'XX-XXXX-X_abcd1234', //OPTIONAL - Amazon Cognito User Pool ID
+//       userPoolWebClientId: 'XX-XXXX-X_abcd1234', //OPTIONAL - Amazon Cognito Web Client ID
+//   },
+//   Storage: {
+//       AWSS3: {
+//           bucket: 'awsapplication12b856bccbc54d9e94319a205630292a-javascript', //REQUIRED -  Amazon S3 bucket
+//           region: 'eu-west-2', //OPTIONAL -  Amazon service region
+//       }
+//   }
+// });
 
 if (environment.production) {
   enableProdMode();
