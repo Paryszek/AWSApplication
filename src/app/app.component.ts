ba5da88a6d9bf8bfd00a61e90d78375583e63bdb
Michal Parysz <michal.parysz@r.pl>
2019-06-12 17:07:32 +0200

    tabela dla listy plikow

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-
+class FileInfo {
+  ETag: string;
+  Key: string;
+  LastModified: string;
+  Size: number;
+  StorageClass: string;
+}
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -9,8 +15,10 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 export class AppComponent {
   title: string = "AWS test application";
   file: File;
-
-  constructor(private http: HttpClient) {}
+  files: FileInfo[] = [];
+  constructor(private http: HttpClient) {
+    this.initFiles();
+  }
 
   onImageLoaded(event: EventTarget) {
     let eventObj: MSInputMethodContext = <MSInputMethodContext> event;
@@ -28,7 +36,14 @@ export class AppComponent {
 
     let headers = new HttpHeaders();
     headers.append('Content-Type', 'multipart/form-data');
-    this.http.post('http://localhost:4000/', formData, { headers }).subscribe(data => console.log(data));
-    console.log(this.file);
+    this.http.post('http://localhost:4000/upload', formData, { headers }).subscribe((data: any) => this.initFiles());
+  }
+
+  initFiles() {
+    this.http.post('http://localhost:4000/files', undefined).subscribe((data: any) => {
+      this.files.length = 0;
+      data.Contents.forEach(file => this.files.push(file));
+      console.log(this.files);
+    });
   }
 }
